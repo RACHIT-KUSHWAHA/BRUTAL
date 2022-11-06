e89e09ff9d2ad832cbb749aacbc58c2c05b2958d
ᴀᴅɪᴛʏᴀ ᴊʜᴀ <105626372+Adityakjha1@users.noreply.github.com>
2022-11-06 09:13:59 +0530

    Test
    Test kr rha hu bsdk

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -93,7 +93,7 @@ PM_START_TEXT = """
 buttons = [
     [
         InlineKeyboardButton(
-            text="ᴀᴅᴅ ᴍᴇ ᴇʟsᴇ ʏᴏᴜ ɢᴇʏ",
+            text="➕ ᴀᴅᴅ ᴍᴇ ᴇʟsᴇ ʏᴏᴜ ɢᴇʏ ➕",
             url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
         ),
     ],
